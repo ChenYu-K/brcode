cee89d28304ce0158c2efbc1c506eb2e03bf9255
Yu Chen <cy519234505@gmail.com>
2021-11-30 17:18:39 +0900

    tran_v1.0

--- Matlab/膜厚計測自動変換/shirai-makuatsu/source/trans.m
+++ Matlab/膜厚計測自動変換/shirai-makuatsu/source/trans.m
@@ -1,5 +1,3 @@
-function  output= trans(model,num)
-
 file = dir(fullfile('*.csv'));      %csvファイルの情報を全部読み取り
 filenames = {file.name};            %csvファイルの名前を取得
 [~,n] = size(filenames);            %csvファイルの個数を数える
@@ -12,12 +10,9 @@ for i = 1 : n
     output{i,2} = readmatrix(k{1,1});     %データを入れる
     f = output{i,2};                      %data中のi行2列のデータを取出す
     f = f(:,3);                         %計測データの3列を取出す
-    if model == 0
-        f = reshape(f,num,[]);                %行列変換（５行ｎ列）
-    else
-        f = reshape(f,[],num);              %行列変換（n行5列）
-    end
+    f = reshape(f,5,[]);                %行列変換（５行ｎ列）
     output{i,2} = f;
+    outputname=append('op_',k{1,1},'.csv');
+    writematrix(f,outputname);
 end
-writematrix(f, 'output.csv');
 clearvars -except output
